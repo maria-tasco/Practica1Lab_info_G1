e4392bc3378200ec79abb4e115086356fd00349f
Maria Tasco <maria.tasco@udea.edu.co>
2021-03-18 17:42:51 -0500

    Arreglo del numero de problemas y mejorar la salida para que fuera como lo indica el documento

--- main.cpp
+++ main.cpp
@@ -5,20 +5,20 @@ using namespace std;
 int main()
 {
     int numprob=0;
-    cout<<"ingrese el numero del problema: "<<endl; cin>>numprob;
+    cout<<"ingrese el numero del ejercicio: "<<endl; cin>>numprob;
     switch (numprob) {
         case 1:{
-             cout<<"Ejercicio 1 Escriba un programa que pida dos números A y B e imprima en pantalla el residuo de la división A/B"<<endl;
+             cout<<"Escriba un programa que pida dos números A y B e imprima en pantalla el residuo de la division A/B"<<endl;
              int a=0,b=0, div=0;
              cout<<"Ingrese el primer numero"<<endl; cin>>a;
              cout<<"Ingrese el segundo numero"<<endl; cin>>b;
              div=a/b;
-             cout<<div<<endl;
+             cout<<"El residuo de la division "<<a<<"/"<<b<<" es: "<<div<<endl;
              break;
         }
         case 2:
         {
-            cout<<"Ejercicio 2 Escriba un programa que pida un numero N e imprima en pantalla si es par o impar."<<endl;
+            cout<<"Escriba un programa que pida un numero N e imprima en pantalla si es par o impar."<<endl;
             int n=0;
             cout<<"Ingrese un numero: "<<endl; cin>>n;
             if(n%2==0)
@@ -29,25 +29,32 @@ int main()
         }
         case 3:
         {
-            cout<<"Ejercicio 3 Escriba un programa que pida dos números A y B e imprima en pantalla el mayor."<<endl;
+            cout<<"Escriba un programa que pida dos números A y B e imprima en pantalla el mayor."<<endl;
             int a=0,b=0;
             cout<<"ingrese 2 numeros: "<<endl; cin>>a>>b;
             if(a>b)
-                cout<<a<<"es mayor"<<endl;
+                cout<<"El mayor es "<<a<<endl;
             else if(a==b)
                 cout<<b<<" y "<<b<<" son iguales"<<endl;
             else
-                cout<<b<<" es mayor"<<endl;
+                cout<<"El mayor es "<<b<<endl;
             break;
         }
         case 4:
+        {
+            cout<<"Escriba un programa que pida dos numeros A y B e imprima en pantalla el menor."<<endl;
+
+            break;
+        }
+
+        case 5:
         {
             cout<<"Escriba un programa que pida dos números A y B e imprima en pantalla la división A/B con redondeo."<<endl;
             int a=0, b=0, div=0;
             cout<<"ingrese dos numeros"<<endl; cin>>a>>b;
             break;
         }
-        case 5:
+        case 6:
         {
             cout<<"Escriba un programa que pida dos números A y B e imprima en pantalla la potencia A^B"<<endl;
             int a=0, b=0;
@@ -60,7 +67,7 @@ int main()
             cout<<c<<"^"<<b<<"="<<a<<endl;
             break;
         }
-        case 6:
+        case 7:
         {
             cout<<"Escriba un programa que pida un número N e imprima en pantalla la suma de todos los números entre 0 y N (incluyéndose el mismo)"<<endl;
             int n=0, suma=0; //¿? Cómo hago con los números negativos, usando unsigned int no compila.
@@ -75,19 +82,19 @@ int main()
             break;
 
         }
-        case 7:
+        case 8:
         {
-            cout<<"Escriba un programa que pida un número N e imprima en pantalla su factorial"<<endl;
+            cout<<"Escriba un programa que pida un numero N e imprima en pantalla su factorial"<<endl;
             int n=0, factorial=1;
             cout<<"ingrese un numero"<<endl; cin>>n;
             for(int i=1;i<=n;i++)
                 factorial*=i;
-            cout<<factorial<<endl;
+            cout<<n<<"!="<<factorial<<endl;
             break;
         }
-        case 8:
+        case 9:
         {
-            cout<<"Escriba un programa que pida un número N e imprima en pantalla todos los multiplos de dicho número entre 1 y 100"<<endl;
+            cout<<"Escriba un programa que pida un número N e imprima el perímetro y área de uncírculo con radio N."<<endl;
 
 
         }
